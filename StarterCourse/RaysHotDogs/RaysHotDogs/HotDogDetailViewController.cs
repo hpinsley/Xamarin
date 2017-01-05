2158c11107173d2659eee6ff57c64be35b3286ee
Howard Pinsley <hpinsley@gmail.com>
2017-01-04 22:41:29 -0500

    Displays the image on the first view

--- StarterCourse/RaysHotDogs/RaysHotDogs/HotDogDetailViewController.cs
+++ StarterCourse/RaysHotDogs/RaysHotDogs/HotDogDetailViewController.cs
@@ -11,7 +11,39 @@ namespace RaysHotDogs
 
         public HotDogDetailViewController (IntPtr handle) : base (handle)
         {
-			
+			HotDogDataService hotDocDataService = new HotDogDataService();
+			this.SelectedHotDog = hotDocDataService.GetHotDogById(1);
         }
-    }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			DatabindUI();
+
+			//AddToCartButton.TouchUpInside += (object sender, EventArgs e) =>
+			//{
+
+			//	UIAlertView message = new UIAlertView("Ray's Hot Dogs", "That hot dog was added to your order", null, "OK", null);
+			//	message.Show();
+
+			//};
+
+			//CancelButton.TouchUpInside += (object sender, EventArgs e) =>
+			//{
+			//	//we'll come back this one later
+			//};
+
+		}
+
+		private void DatabindUI()
+		{
+			UIImage img = UIImage.FromFile("Images/" + SelectedHotDog.ImagePath + ".jpg");
+			HotDogImageView.Image = img;
+			//NameLabel.Text = SelectedHotDog.Name;
+			//ShortDescriptionLabel.Text = SelectedHotDog.ShortDescription;
+			//LongDescriptionText.Text = SelectedHotDog.Description;
+			//PriceLabel.Text = "$" + SelectedHotDog.Price.ToString();
+		}
+	}
 }
