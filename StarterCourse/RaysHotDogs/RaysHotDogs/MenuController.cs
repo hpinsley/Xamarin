3890033220c2c875e806a86e3345fb0cd8be9d38
Howard Pinsley <hpinsley@gmail.com>
2017-01-07 23:14:37 -0500

    Navigation starting to work

---
+++ StarterCourse/RaysHotDogs/RaysHotDogs/MenuController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace RaysHotDogs
+{
+    public partial class MenuController : UIViewController
+    {
+        public MenuController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
